1d42619891f60f3b67a85030f494ea56ae57bce7
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-19 11:21:48 +0530

    remove

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
 
 export default defineConfig({
   plugins: [react()], // or ssr(['/']) if you're specifying routes
