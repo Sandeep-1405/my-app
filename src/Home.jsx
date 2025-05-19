c2f6d29ef4d1d7ed6427671a74617a69f2e78246
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-19 10:43:35 +0530

    add hme

---
+++ src/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+        <meta property="og:title" content="Your Website Title" />
+        <meta
+          property="og:description"
+          content="A short description of your website or page."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta
+          property="og:image"
+          content="https://omegaphysicians.net/assets/opengraphic-DY9TuWir.jpg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Your Website Title" />
+        <meta
+          name="twitter:description"
+          content="A short description of your website or page."
+        />
+        <meta
+          name="twitter:image"
+          content="https://omegaphysicians.net/assets/opengraphic-DY9TuWir.jpg"
+        />
+      </Helmet>
+      <h1>Home</h1>
+      <Link to="/settings">Go to settings</Link>
+    </div>
+  );
+};
+
+export default Home;
